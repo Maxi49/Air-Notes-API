2c52587e86f95b72a172191379cc3b871fdf33f1
Frame <gimenezzmaximiliano@gmail.com>
2024-04-13 18:49:28 -0300

    PORT

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

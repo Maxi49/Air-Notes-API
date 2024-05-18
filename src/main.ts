43c2b9e5223416ad1dad845c52321bb241b9cba8
Frame <gimenezzmaximiliano@gmail.com>
2024-05-18 19:20:46 -0300

    env logs added

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './ErrorHandlers/errorHandlers';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  await app.listen(process.env.PORT || 10020);
+    const { httpAdapter } = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors();
+    console.log(process.env);
+    console.log(process.env.PORT);
+    await app.listen(process.env.PORT || 8080);
+  } catch (error) {
+    throw new HttpException('unhandled exception', 500, error);
+  }
 }
 bootstrap();
